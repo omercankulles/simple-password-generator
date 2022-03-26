f18eeb14a7ae1f70e0c12960c287ac40577ea15f
omercankulles <omercankulles@gmail.com>
2022-03-26 16:02:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -6,15 +6,43 @@ function App() {
 
   const [passwordLength, setPasswordLength] = useState(6);
 
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
 
-  const handleSliderChange = (es) => {
+  const [passwords,setPasswords] = useState([]);
+
+  const [btnDisabled, setBtnDisabled] = useState(false);
+
+  const handleSliderChange = (e) => {
     
     setPasswordLength(e.target.value);
 
     const generatedPassword = generatePassword(e.target.value);
 
     setPassword(generatedPassword);
+
+    setBtnDisabled(false);
+
+  }
+
+  const setPasswordLengthColor = (pwLength) => {
+
+    if(!pwLength) pwLength = passwordLength;
+
+    if(pwLength < 11) {
+      return 'bg-danger';
+    } else if(pwLength >=11 && pwLength<20) {
+      return 'bg-warning';
+    } else {
+      return 'bg-success';
+    }
+  }
+
+  const handleSaveButtonClick = (e) => {
+
+    setPasswords([...passwords, password]);
+
+    setBtnDisabled(true);
+  
   }
 
   return (
@@ -22,7 +50,6 @@ function App() {
       <div className='row'>  
         <div className='col-12'>
           <div className='d-flex flex-column justify-content-center align-items-center'>
-            
             <div className='card mt-3'>
               <div className='card-body'>
                 <div className='card-title text-center'>
@@ -33,7 +60,7 @@ function App() {
                 </div>
                 <div className='mt-2'>
                   <label className='form-label' htmlFor='password-length-slider'>
-                    Password Length <span className='badge bg-success'>{passwordLength}</span>
+                    Password Length <span className={`badge ${setPasswordLengthColor()}`}>{passwordLength}</span>
                   </label>
                   <input 
                     id='password-length-slider'
@@ -52,26 +79,29 @@ function App() {
                     type="text" 
                     style={{cursor:'pointer'}}
                     value={password}
+                    readOnly={true}
                   />
-                  <button className='btn btn-info mt-3'>Save</button>
+                  <button className='btn btn-info mt-3' disabled={btnDisabled} onClick={(e)=>handleSaveButtonClick(e)}>Save</button>
                   <button className='btn btn-outline-info mt-3 float-end'>Reset Saved Password</button>
                 </div>
               </div>
             </div>
-        
             <div className='card mt-3'>
               <div className='card-body'>
                 <div className='card-header text-center'>
                   <h2 className='text-info'>Recent Generated Passwords</h2>
                 </div>
                 <ul className='list-group list-group-flush text-center'>
-                  <li className='list-group-item'><span className='fst-italic float-start'>1</span>
-                  <div className='badge bg-success' style={{cursor:'pointer'}}>1</div>
+                  {passwords.map((password,index) => (
+                  <li key={index} className='list-group-item'><span className='fst-italic float-start'>{ index + 1 }</span>
+                    <div className={`badge ${setPasswordLengthColor(password.length)}`} style={{cursor:'pointer'}}>
+                      {password}
+                    </div>
                   </li>
+                  ))}
                 </ul>
               </div>
             </div>
-
           </div>
         </div>
       </div>
